baf2799e77b4d8d7908181098efe316c4ad1408f
Rahul Vikram <75101301+rahulvikram@users.noreply.github.com>
2025-04-05 14:30:54 -0700

    Set up sample routes with nextjs routing

---
+++ src/app/core/cooking-chatbot/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function CookingChatbot() {
+    return <div>
+        <h1>Cooking Chatbot</h1>
+        <Image 
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            width={100}
+            height={100}
+        />
+    </div>
+}
+
